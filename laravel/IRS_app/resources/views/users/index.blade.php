2f411c44ca932af64fdddec953a32b5633948e49
RJSkudra <skudru.rinalds@gmail.com>
2025-02-19 23:36:05 +0200

    Add favicon link to user index page for improved branding

--- laravel/IRS_app/resources/views/users/index.blade.php
+++ laravel/IRS_app/resources/views/users/index.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>IRS datu ievade</title>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="icon" href="https://img.icons8.com/ultraviolet/80/data-configuration.png" type="image/png">
 </head>
 <body>
     <div class="form-container">
